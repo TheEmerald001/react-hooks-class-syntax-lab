b42c6286fe949f864dba230fdd2b3f6041a2e62b
steve <austines811@gmail.com>
2023-01-19 01:36:17 +0300

    refactor functional components as class components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+// function NavBar() {
+//   return (
+    
+//   );
+// }
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    )
+  }
 }
 
-export default NavBar;
+
+export default Navbar;
